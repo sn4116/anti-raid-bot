a75cd23ab63249ae55ef220a397a9085e88967a5
Msv <78690237+MSVFORYOU@users.noreply.github.com>
2021-05-19 22:25:07 +0530

    Updated : )

---
+++ config.py
@@ -0,0 +1,20 @@
+#Config:#
+token = 'REPLACE_TOKEN'                
+CommandPrefix = 'REPLACE_PREFIX'    
+activitytype = 'Playing'                        #Accepted Values are Watching, Listening, Playing, or Streaming.
+botstatusmessage = 'REPLACE_STATUS_HERE'        
+developerid = 0000000000000000000               #Insert the userID here were the bot will send all error dms too. THE USER MUST BE IN THE SAME DISCORD SERVER THAT THE BOT IS IN!
+guildID = 0000000000000000000                   #Insert the guildID of the server that the bot will be running in here.
+StaffRoleID = 0000000000000000000               #Change this value to the role in the server that will give bot perms to the anti-raid commands
+SystemLogsChannelID = 0000000000000000000       #Change this value to where the bot will send all logs of commands too
+
+#Embeds#
+embedcolor = 0x000000                           #Change this value to the color value of the hex code that you would like the embeds the bot sends to be (KEEP THE 0x!)
+
+customfooter = False                            #Set this the True if you like to have a custom footer at embeds            
+customfootvalue = ''                            #Place the text of the custom footers
+
+#Anti-Raid:
+GracePeriodForKicks = 600                       #Adjust this value for the bot to look for members to kick who joined during a certain amount of time before anti-raid is enabled, default is 10 minutes, seconds only 
+IncludeInviteLink = False                       #Change this to True for Yes, False for No. This allows for a discord invite to be on the message that the bot sends to a member who is kicked due to anti-raid
+DiscordServerInviteLink = ''                    #Place in here the link to the discord. Use https, for example: https://discord.gg/yourvanityurl
